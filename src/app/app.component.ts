483e97020c1e35e2f10d2a4b5bf630fd068cd023
AndreTrevizam <andretrevizam264@gmail.com>
2025-06-10 19:20:16 -0300

    Refactor app component to integrate base UI and certificates components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
 import { CertificateItemComponent } from './_components/certificate-item/certificate-item.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertificateItemComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertificateItemComponent, BaseUiComponent, CertificatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
