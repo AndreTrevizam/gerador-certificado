ef1ad164589e959d47868a213b81f4ef928badd3
AndreTrevizam <andretrevizam264@gmail.com>
2025-06-14 16:44:44 -0300

    Remove unused certificate components and update app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
-import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
-import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
-import { CertificateItemComponent } from './_components/certificate-item/certificate-item.component';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
-import { CertificatesComponent } from './pages/certificates/certificates.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertificateItemComponent, BaseUiComponent, CertificatesComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
